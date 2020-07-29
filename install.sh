d81a221581db79391669bbc733d4104fba0adb4c
Christopher Grant <christopher.grant@protonmail.com>
2020-07-29 15:46:32 -0700

    init

---
+++ install.sh
@@ -0,0 +1,12 @@
+REDASH_DOCKER_HOME=/opt/redash
+
+git clone https://github.com/getredash/redash.git
+cd redash/
+git checkout release/9.0.x
+sudo su
+docker build --pull=true --build-arg databricks_odbc_driver_url="https://databricks.com/wp-content/uploads/2.6.15.1018/SimbaSparkODBC-2.6.15.1018-Debian-64bit.zip" -t redash/redash:9.0.x-new-odbc .
+
+mv new_docker-compose.yml $REDASH_DOCKER_HOME/docker-compose.yml
+docker-compose up --force-recreate --build
+
+exit
