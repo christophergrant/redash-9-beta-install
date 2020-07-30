959bdd26a0d719b573ead0c799f9458027de2316
Christopher Grant <christopher.grant@protonmail.com>
2020-07-30 13:20:21 -0700

    add comments, change default directories

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+set -eux
+
 REDASH_DOCKER_HOME=/opt/redash
 
 git clone https://github.com/getredash/redash.git
 cd redash/
 git checkout release/9.0.x
-sudo su
 docker build --pull=true --build-arg databricks_odbc_driver_url="https://databricks.com/wp-content/uploads/2.6.15.1018/SimbaSparkODBC-2.6.15.1018-Debian-64bit.zip" -t redash/redash:9.0.x-new-odbc .
 
-mv new_docker-compose.yml $REDASH_DOCKER_HOME/docker-compose.yml
-docker-compose up --force-recreate --build
-
-exit
+# docker compose yaml edited to use the new odbc image, as well as migrate from v8 to v9
+# https://github.com/getredash/redash/releases/tag/v9.0.0-beta
+cp -rf ../new_docker-compose.yml $REDASH_DOCKER_HOME/docker-compose.yml
+docker-compose -f $REDASH_DOCKER_HOME/docker-compose.yml up --force-recreate --build -d
