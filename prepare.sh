899df504b1e755158cd93835c91ac8693c1b7c9f
Christopher Grant <christopher.grant@protonmail.com>
2020-09-15 11:41:28 -0700

    static_saml INIT

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
 
-git clone https://github.com/getredash/redash.git
+git clone https://github.com/christophergrant/redash.git
 cd redash/
-git checkout release/9.0.x
+#git checkout release/9.0.x
 
 cd ..
 cp -f data/Dockerfile redash/
