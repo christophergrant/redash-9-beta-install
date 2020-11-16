dc98f020e9485711d3ffe59a5557c5a745f72fb3
Christopher Grant <christopher.grant@protonmail.com>
2020-11-16 14:38:28 -0800

    Use wildcarding for adding certs to the Docker image, propagate certs from parent directory into the redash stuff

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -eux
 
+# Download the repo
 git clone https://github.com/getredash/redash.git
+
+# Copy certs for worker SSL into the Redash Docker context so we can bake them into the server image
+mkdir redash/certs
+cp -f ../certs/* redash/certs
+
+# Copy modified Dockerfile into the Redash Docker context
 cp -f data/Dockerfile redash/
