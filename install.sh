fa6aecc0a8255465ac0bc5f1eb4ec1a455b56e0d
Christopher Grant <christopher.grant@protonmail.com>
2020-11-16 15:26:55 -0800

    Bifurcate logic for cert/nocert setup

--- install.sh
+++ install.sh
@@ -7,11 +7,15 @@ REDASH_DOCKER_HOME=/opt/redash
 git clone https://github.com/getredash/redash.git
 
 # Copy certs for worker SSL into the Redash Docker context so we can bake them into the server image
-mkdir redash/certs
-cp -f /certs/* redash/certs
-
-# Copy modified Dockerfile into the Redash Docker context
-cp -f data/Dockerfile redash/
+if ls certs/*.crt 2> /dev/null; then
+    echo "Found certs, adding to Docker context"
+    mkdir redash/certs
+    cp -f certs/* redash/certs
+    # Copy modified Dockerfile into the Redash Docker context
+    cp -f data/Dockerfile redash/
+else
+    echo "No self-signed certs to add"
+fi
 
 cd redash/
 docker build --pull=true --build-arg databricks_odbc_driver_url="https://databricks.com/wp-content/uploads/2.6.15.1018/SimbaSparkODBC-2.6.15.1018-Debian-64bit.zip" -t redash/redash:9.0.x-new-odbc .
